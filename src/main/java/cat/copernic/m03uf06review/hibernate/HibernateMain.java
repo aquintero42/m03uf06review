cd4b92b3a7bdc650b10b21551e021a3d09b8453e
Pep <pep.mendez@copernic.cat>
2020-11-24 20:19:00 +0100

    project ready to download

--- src/main/java/cat/copernic/m03uf06review/hibernate/HibernateMain.java
+++ src/main/java/cat/copernic/m03uf06review/hibernate/HibernateMain.java
@@ -3,19 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cat.copernic.m03uf06review.jdbc;
+package cat.copernic.m03uf06review.hibernate;
 
 /**
  * En aquesta secció cal accedir a una taula de MySQL amb un camp de cada tipus:
  * 
  * int o long, double o float, boolean, char, String i java.sql.Date
  * 
- * Recòrrer el result set i mostrar-lo per la consola.
+ * Recòrrer el result set i mostrar-lo per la consola com a instancies de la 
+ * classe Registre, que tindrà l'estructura de la teva taula.
+ * 
+ * Cal usar la llibreria Hibernate.
  * 
  * 
  * @author pep
  */
-public class JdbcMain {
+public class HibernateMain {
 
     /**
      * @param args the command line arguments
