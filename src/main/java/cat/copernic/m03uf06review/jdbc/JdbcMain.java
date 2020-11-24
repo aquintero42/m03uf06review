b6d90deddda6cfea8c376a32b3b11a3cc8481ebc
Pep <pep.mendez@copernic.cat>
2020-11-24 19:57:58 +0100

    Initial commig

---
+++ src/main/java/cat/copernic/m03uf06review/jdbc/JdbcMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.copernic.m03uf06review.jdbc;
+
+/**
+ *
+ * @author pep
+ */
+public class JdbcMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
